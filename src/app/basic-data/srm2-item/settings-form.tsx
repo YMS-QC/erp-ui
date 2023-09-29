6b7adf8d1ca6428634ddd9c57f7fc9803eceb246
YMS-QC <quacimodoxz@gmail.com>
2023-09-29 21:20:34 +0800

    add

---
+++ src/app/basic-data/srm2-item/settings-form.tsx
@@ -0,0 +1,122 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { format } from 'date-fns';
+
+import { CalendarIcon } from '@radix-ui/react-icons';
+
+import { Calendar } from '@/components/ui/calendar';
+
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { toast } from '@/components/ui/use-toast';
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+
+const FormSchema = z.object({
+    username: z.string().min(2, {
+        message: 'Username must be at least 2 characters.',
+    }),
+    dob: z.date({
+        required_error: 'A date of birth is required.',
+    }),
+});
+
+export function SettingsForm() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        toast({
+            title: 'You submitted the following values:',
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4 z-50">
+                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+                </pre>
+            ),
+        });
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 ">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>This is your public display name.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="dob"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col w-full">
+                            <FormLabel>Date of birth</FormLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                            variant="outline"
+                                            className={cn(
+                                                'w-auto pl-3 text-left font-normal',
+                                                !field.value && 'text-muted-foreground',
+                                            )}
+                                        >
+                                            {field.value ? (
+                                                format(field.value, 'PPP')
+                                            ) : (
+                                                <span>Pick a date</span>
+                                            )}
+                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        disabled={(date: any) =>
+                                            date > new Date() || date < new Date('1900-01-01')
+                                        }
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                            <FormDescription>
+                                Your date of birth is used to calculate your age.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="p-2">
+                    <Button type="submit">提交</Button>
+                </div>
+            </form>
+        </Form>
+    );
+}
